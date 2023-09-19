6a6a198977f5548c46f361b42d7425a16b6a420c
Priscilla Mendez <priapontem@gmail.com>
2023-09-19 10:41:18 -0400

    2nd commit

--- js/main.js
+++ js/main.js
@@ -6,6 +6,40 @@ console.log(document.getElementsByTagName('h1')[0].innerHTML)
 const apiKey = `ac63683ebb9277479c6261c51a63898f`
 const apiURL = "http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid=ac63683ebb9277479c6261c51a63898f"
 
+const addWeather = (temp_max,temp_min,forecast,humidity) => {
+    const html = `<div class="WeatherInfo">
+    <div class="high_card mx-auto mb-5" style="width: 25rem;">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item bg-danger">High</li>
+        <li class="list-group-item">${temp_max}</li>
+        </ul>
+    </div>
+    <div class="low_card mx-auto mb-5" style="width: 25rem;">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item bg-success">Low</li>
+        <li class="list-group-item">${temp_min}</li>
+        </ul>
+    </div>
+    <div class="forecast_card mx-auto mb-5" style="width: 25rem;">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item bg-primary">Forecast</li>
+        <li class="list-group-item">${forecast}</li>
+        </ul>
+    </div>
+    <div class="humidity_card mx-auto mb-5" style="width: 25rem;">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item bg-warning">Humidity</li>
+        <li class="list-group-item">${humidity}</li>
+        </ul>
+    </div>
+    </div>
+    `;
+let card = document.getElementById("row1");
+card.insertAdjacentHTML("afterbegin", html)
+
+}
+
+
 const getData = async (city,state,country)  =>{
     let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=ac63683ebb9277479c6261c51a63898f`)
    
@@ -35,46 +69,52 @@ const getWeather = async (lat,lon) => {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ac63683ebb9277479c6261c51a63898f&units=imperial`)
     let data = await response.json()
     console.log(data)
+    addWeather(data.main.temp_max,data.main.temp_min,data)
 }
 
 
-const addWeather = (temp_max,temp_min,forecast,humidity) => {
-    const html = `<div class="WeatherInfo">
-    <div class="high_card mx-auto mb-5" style="width: 25rem;">
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item bg-danger">High</li>
-        <li class="list-group-item">${temp_max}</li>
-        </ul>
-    </div>
-    <div class="low_card mx-auto mb-5" style="width: 25rem;">
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item bg-success">Low</li>
-        <li class="list-group-item">${temp_min}</li>
-        </ul>
-    </div>
-    <div class="forecast_card mx-auto mb-5" style="width: 25rem;">
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item bg-primary">Forecast</li>
-        <li class="list-group-item">${forecast}</li>
-        </ul>
-    </div>
-    <div class="humidity_card mx-auto mb-5" style="width: 25rem;">
-        <ul class="list-group list-group-flush">
-        <li class="list-group-item bg-warning">Humidity</li>
-        <li class="list-group-item">${humidity}</li>
-        </ul>
-    </div>
-    </div>
-    `
-}
- const loadData = async() => {}
-    const weather = await getData() 
-    let queryHigh = document.querySelector("temp_max")
-    let queryLow = document.querySelector("temp_min")
-    let queryForecast = document.querySelector("forecast")
-    let queryHumidity = document.querySelector("humidity")
+// const addWeather = (temp_max,temp_min,forecast,humidity) => {
+//     const html = `<div class="WeatherInfo">
+//     <div class="high_card mx-auto mb-5" style="width: 25rem;">
+//         <ul class="list-group list-group-flush">
+//         <li class="list-group-item bg-danger">High</li>
+//         <li class="list-group-item">${temp_max}</li>
+//         </ul>
+//     </div>
+//     <div class="low_card mx-auto mb-5" style="width: 25rem;">
+//         <ul class="list-group list-group-flush">
+//         <li class="list-group-item bg-success">Low</li>
+//         <li class="list-group-item">${temp_min}</li>
+//         </ul>
+//     </div>
+//     <div class="forecast_card mx-auto mb-5" style="width: 25rem;">
+//         <ul class="list-group list-group-flush">
+//         <li class="list-group-item bg-primary">Forecast</li>
+//         <li class="list-group-item">${forecast}</li>
+//         </ul>
+//     </div>
+//     <div class="humidity_card mx-auto mb-5" style="width: 25rem;">
+//         <ul class="list-group list-group-flush">
+//         <li class="list-group-item bg-warning">Humidity</li>
+//         <li class="list-group-item">${humidity}</li>
+//         </ul>
+//     </div>
+//     </div>
+//     `;
+// let card = document.getElementById("row1");
+// card.insertAdjacentHTML("afterbegin", html)
 
-    weather.forEach(element => )
+// }
+
+
+//  const loadData = async() => {}
+//     const weather = await getData() 
+//     let queryHigh = document.querySelector("temp_max")
+//     let queryLow = document.querySelector("temp_min")
+//     let queryForecast = document.querySelector("forecast")
+//     let queryHumidity = document.querySelector("humidity")
+
+//     weather.forEach(element => )
 // get lat and lon values to plug into this ^
 
 // make new function that takes in lon and lat and then 
